6405edd54def61d63aea1874a70ff49f53534cb6
Dan <florentius4@gmail.com>
2022-10-04 15:26:13 -0600

    transfer

---
+++ script.js
@@ -0,0 +1,13 @@
+function styleMe(){
+    var grid = document.getElementById('grid');
+    if (grid.style.color == 'white'){
+        grid.style.color = 'red';
+    }
+    else{
+        grid.style.color = 'white';
+    }
+}
+
+function who(){
+    document.getElementById('result').value = 'Leonardo Da Vinci'
+}
